984eec9a0cb6047000dda98c03696592406154a8
asm128 <pabloarielz@outlook.com>
2022-02-18 01:03:23 -0300

    Upgraded to VS2022

--- Tutorial0/main.cpp
+++ Tutorial0/main.cpp
@@ -1,6 +1,6 @@
 // (Visual Studio 2017 has a ZOOM feature which allows to see more code, the recommended setting is exactly 81% but if you like to see more code you can shrink it even more, maybe to 75% as I do sometimes)
-// 
-// A function is defined as follows, where the text between brackets would be what you would replace with your actual code: 
+//
+// A function is defined as follows, where the text between brackets would be what you would replace with your actual code:
 //
 // [returnType]  [functionName]		([functionParameters])		{ [codeBlock]; }
 //
@@ -16,8 +16,8 @@
 //
 // int	beCreative(int argc, char* argv)	{ return 0; }
 // ------------------------------------------------------------------------------------------------------------------
-
-int	main	()	// entry point function with void return value (no return value) and empty parameter list between parentheses. The program code starts executing from here (from the main() function) when the operating system cranks up the .exe
-{ // block of program code between curly braces {}
-	return 0; 
+//
+int		main	()	// entry point function with void return value (no return value) and empty parameter list between parentheses. The program code starts executing from here (from the main() function) when the operating system cranks up the .exe
+{	// block of program code between curly braces {}
+	return 0;
 }
